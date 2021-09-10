a56dd49516be8cd9b3ddb58aa3065b62095f4bed
Marcus <mamerikanos@icloud.com>
2021-09-10 15:01:55 +0300

    confusion matrix and evaluation

--- seqmodel.py
+++ seqmodel.py
@@ -9,6 +9,7 @@ from keras.layers import MaxPool1D
 from keras.layers import Flatten
 from keras.layers import Dropout
 import tensorflow as tf
+import numpy as np
 
 
 class seqmodel:
@@ -36,9 +37,39 @@ class seqmodel:
     def trainmodel(self,numepochs,batchsize,train,trainlabels,val,vallabels):
         tracker = self.model.fit(x=train,y=trainlabels,epochs=numepochs,batch_size=batchsize,validation_data=(val,vallabels))
         return tracker
+
+    def modeleval(self,valdata,vallabels):
+        loss, metric = self.model.evaluate(valdata,vallabels)
+        return(loss,metric)
     
     def predict(self,data):
         preds=self.model.predict(data)
         return preds
     
+    def  confusionmatrix(self,preds,labels,dsthresh):
+        self.truepositives = 0
+        self.truenegatives = 0
+        self.falsepositives = 0
+        self.falsenegatives = 0
+        # prediction to label
+        preds = np.where(preds>dsthresh,1,0)
+        # calculate tp,tn,fp,fn
+        for i,pred in enumerate(preds):
+            label = labels[i]
+            if pred == 1:
+                if label == 1:
+                    self.truepositives+=1
+                else:
+                    self.falsepositives+=1
+            elif pred == 0:
+                if label==0:
+                    self.truenegatives+=1
+                else:
+                    self.falsenegatives+=1
+        
+        # precision,recall,fscore
+        precision = self.truepositives/(self.truepositives+self.falsepositives)
+        recall = self.truepositives/(self.truepositives+self.falsenegatives)
+        fscore = 2*precision*recall/(precision+recall)
+        return (precision, recall, fscore)
     
